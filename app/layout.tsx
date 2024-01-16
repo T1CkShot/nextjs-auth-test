e63332f7ce6950d0e170f90d3ef4bf3ee739ef02
T1CkShot <fatta.mmg@gmail.com>
2024-01-16 23:03:00 +0600

    structure and add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ModeChange } from "@/components/ModeChange";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,16 +18,54 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="w-full flex flex-col items-center {inter.className}">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <NavBar />
+          <div className="w-full flex justify-center">
+            <Container hasPadding>{children}</Container>
+          </div>
         </ThemeProvider>
       </body>
     </html>
+  );
+}
+
+export function NavBar() {
+  return (
+    <header className="w-full border-b border-border/40 flex py-2 sticky justify-center">
+      <Container>
+        <div className="flex items-between">
+          <div className="w-full flex items-center">
+            <h1 className="font-bold text-lg">Auth Test</h1>
+          </div>
+          <ModeChange />
+        </div>
+      </Container>
+    </header>
+  );
+}
+
+export function Container({
+  children,
+  margin,
+  hasPadding = false,
+}: {
+  children: React.ReactNode;
+  margin: string;
+  hasPadding?: boolean;
+}) {
+  return (
+    <div
+      className={`w-full max-w-7xl mx-${margin} ${
+        hasPadding ? "px-2" : "px-0"
+      }`}
+    >
+      {children}
+    </div>
   );
 }
