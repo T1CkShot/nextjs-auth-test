feb10deff555e9a41caec698f89f56eab8c982cf
T1CkShot <fatta.mmg@gmail.com>
2024-01-17 21:14:57 +0600

    fix lower case options error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import Github from "next-auth/providers/github";
 
-export const options: NextAuthOptions = {
+export const OPTIONS: NextAuthOptions = {
   providers: [
     Github({
       clientId: process.env.GITHUB_CLIENT_ID as string,
@@ -11,6 +11,6 @@ export const options: NextAuthOptions = {
   ],
 };
 
-const handler = NextAuth(options);
+const handler = NextAuth(OPTIONS);
 
 export { handler as GET, handler as POST };
